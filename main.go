12c327bb1c363932186c3b937eadb76435bab54c
rovezuka <rostislav.vetoshkin@gmail.com>
2023-09-19 18:47:48 +0300

    Добавлены функции просмотра текущей директории пользователя и генерации кода в указанный файл по шаблону из папки templates с введенными данными по флагам

--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
+	"text/template"
 
 	"github.com/spf13/cobra"
 )
@@ -122,6 +124,82 @@ func main() {
 		},
 	}
 	rootCmd.AddCommand(deleteDirCmd)
+
+	var generateCmd = &cobra.Command{
+		Use:   "generate [template] [output] --name [name]",
+		Short: "Generate code based on template",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			templateName := args[0]
+			outputFileName := args[1]
+			name, _ := cmd.Flags().GetString("name")
+
+			exePath, err := os.Executable()
+			if err != nil {
+				fmt.Printf("Failed to get executable path: %s\n", err)
+				return
+			}
+			executableDir := filepath.Dir(exePath)
+
+			templatePath := filepath.Join(executableDir, "templates", templateName)
+			outputPath := filepath.Join(executableDir, outputFileName)
+
+			// Загрузить содержимое шаблона из файла
+			templateContent, err := ioutil.ReadFile(templatePath)
+			if err != nil {
+				fmt.Printf("Failed to read template file: %s\n", err)
+				return
+			}
+
+			// Создать шаблон и вставить значение в него
+			tmpl, err := template.New("codeTemplate").Parse(string(templateContent))
+			if err != nil {
+				fmt.Printf("Failed to parse template: %s\n", err)
+				return
+			}
+
+			data := struct {
+				Name string
+			}{
+				Name: name,
+			}
+
+			// Создать выходной файл и записать в него результат
+			outputFile, err := os.Create(outputPath)
+			if err != nil {
+				fmt.Printf("Failed to create output file: %s\n", err)
+				return
+			}
+			defer outputFile.Close()
+
+			if err := tmpl.Execute(outputFile, data); err != nil {
+				fmt.Printf("Failed to generate code: %s\n", err)
+				return
+			}
+
+			fmt.Printf("Generated code from template %s to %s\n", templatePath, outputPath)
+		},
+	}
+
+	generateCmd.Flags().String("name", "", "Name to insert into the template")
+
+	// Добавьте эту команду к вашему rootCmd
+	rootCmd.AddCommand(generateCmd)
+
+	var currentDirCmd = &cobra.Command{
+		Use:   "currentdir",
+		Short: "Show the current working directory",
+		Run: func(cmd *cobra.Command, args []string) {
+			currentDir, err := os.Getwd()
+			if err != nil {
+				fmt.Printf("Failed to get current directory: %s\n", err)
+				return
+			}
+			fmt.Printf("Current working directory: %s\n", currentDir)
+		},
+	}
+
+	rootCmd.AddCommand(currentDirCmd)
 
 	if err := rootCmd.Execute(); err != nil {
 		fmt.Println(err)
